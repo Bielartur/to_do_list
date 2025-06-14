655b2dee7903b9ab6d6fec33cb32c5aeb73e0de2
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-14 00:30:32 -0300

    Adicionar e deletar tarefas

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ app_name = 'app'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('tasks/<int:pk>/delete/', Home.as_view(), name='tasks'),
     # path('editarperfil/', EditarPerfil.as_view(), name='editar_perfil'),
 ]
